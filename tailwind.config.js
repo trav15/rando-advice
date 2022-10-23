3d01e33a74d3dedbc77dfae333515d04a42d9142
trav15 <travistokuyama@gmail.com>
2022-10-23 15:24:20 +0100

    Update Tailwind fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      sans: ['Nunito', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
